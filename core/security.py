3e003a2d73738a03db21f934565b944e59819868
Lucianogalizia <lucianogalizia170@hotmail.com>
2026-02-11 22:53:04 -0300

    Update security.py

--- core/security.py
+++ core/security.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from core.settings import settings
 
 def create_access_token(data: dict, expires_minutes: int = 480):
@@ -11,3 +11,14 @@ def create_access_token(data: dict, expires_minutes: int = 480):
         settings.JWT_SECRET,
         algorithm=settings.JWT_ALGORITHM
     )
+
+def decode_token(token: str) -> dict:
+    try:
+        return jwt.decode(
+            token,
+            settings.JWT_SECRET,
+            algorithms=[settings.JWT_ALGORITHM]
+        )
+    except JWTError:
+        return {}
+
